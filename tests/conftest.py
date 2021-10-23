fb331d59860ca01c73ef80d88d7f16c0c7cdde87
kreyoo <zunder325@gmail.com>
2021-10-23 11:18:41 +0200

    formatting with black + poetry for packaging

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
-
 from subprocess import run
+
 try:
     from csgoinvshuffle.inventory import parse_inventory
 except ModuleNotFoundError:
-    raise ModuleNotFoundError('Please run "pip install ." in the projects root directory')
+    raise ModuleNotFoundError(
+        'Please run "pip install ." in the projects root directory'
+    )
 import pytest
 import utils
 
 
 def test_install():
-    assert run(['pip', 'install', '-r', "requirements.txt"]).returncode == 0
-    assert run(['pip', 'install', '.', '--use-feature=in-tree-build']).returncode == 0
+    assert run(["pip", "install", "-r", "requirements.txt"]).returncode == 0
+    assert run(["pip", "install", ".", "--use-feature=in-tree-build"]).returncode == 0
 
 
 inventory = parse_inventory(utils.example_data(), "76561198232352624")
--- tests/utils.py
+++ tests/utils.py
@@ -2,23 +2,23 @@ import json
 
 
 def __read_file(file: str):
-    f = open("tests/test_data/"+file, 'r', encoding='utf-8')
+    f = open("tests/test_data/" + file, "r", encoding="utf-8")
     data = f.read()
     f.close()
     return data
 
 
 def example_data() -> dict:
-    d = __read_file('test_inventory.json')
+    d = __read_file("test_inventory.json")
     return json.loads(d)
 
 
 def example_csgo_saved_item_shuffles() -> str:
-    return __read_file('compare_data.txt')
+    return __read_file("compare_data.txt")
 
 
 def new_shuffleconfig() -> str:
-    f = open("./csgo_saved_item_shuffles.txt", 'r', encoding='utf-8')
+    f = open("./csgo_saved_item_shuffles.txt", "r", encoding="utf-8")
     data = f.read()
     f.close()
     return data
